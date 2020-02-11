c0984c812a86c51c6472d3cf225c0f8e7522cbe8
Krishan Rajaratnam <kr2481@nyu.edu>
2020-02-11 15:45:41 -0500

    movement scripts

---
+++ Game Studio 2/Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Player player;
+    public Vector2Int coord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.tiles[coord.x, coord.y] = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
