d44689f0bb16dd88846a840b25df469f62e0780a
willrhodes <wrhodes42@gmail.com>
2020-02-11 17:08:31 -0500

    added timer, timer UI

---
+++ Game Studio 2/Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public int tau;
+    Text tauText;
+
+    void Start(){
+        tauText = gameObject.GetComponent<Text>();
+    }
+
+    public void AddTime(int timeToAdd){
+        tau += timeToAdd;
+    }
+
+    void Update(){
+        tauText.text = tau.ToString();
+    }
+}
