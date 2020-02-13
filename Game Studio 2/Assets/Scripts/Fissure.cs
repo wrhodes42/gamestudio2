bd7c4a2d093723422161d5fc34b4c678aa2751d4
Will Rhodes <wrhodes42@gmail.com>
2020-02-12 21:10:56 -0500

    sentient plant, fissures

--- Game Studio 2/Assets/Scripts/Fissure.cs
+++ Game Studio 2/Assets/Scripts/Fissure.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class Fissure : Tile
 {
-    List<Transform> allFissures = new List<Transform>();
+    public List<GameObject> allFissures = new List<GameObject>();
 
-    public void Start()
-    {
-        
-    }
+    public bool teleported = false;
 
     public override void OnTileEnter()
     {
-        player.timer.AddTime(1); // swimming takes time i guess
+        player.timer.AddTime(1); // falling takes time i guess
+
+        StartCoroutine(FissureWarp());
     }
 
     public override void OnTileExit()
     {
-        
+        teleported = false;
+    }
+
+    IEnumerator FissureWarp(){
+        yield return new WaitForSeconds(0.5f);
+
+        if(teleported == false){
+
+            //get array of fissures on map, convert to list
+            GameObject[] fissuresArray = GameObject.FindGameObjectsWithTag("Fissure");
+
+            for (int i = 0; i < fissuresArray.Length; i++)
+            {
+                allFissures.Add(fissuresArray[i]);
+            }
+
+            //remove self from list
+            allFissures.Remove(gameObject);
+
+            //pick a random fissure from the set
+            GameObject selectedFissure = allFissures[Random.Range(0, allFissures.Count)];
+
+            player.playerPos = selectedFissure.GetComponent<Fissure>().coord;
+
+            teleported = true;
+            allFissures.Clear();
+        }
     }
 }
