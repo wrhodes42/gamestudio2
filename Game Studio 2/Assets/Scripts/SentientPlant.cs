7b47d2910bea49fd1c989b645bc7a9ef547371a0
Will Rhodes <wrhodes42@gmail.com>
2020-02-13 10:04:28 -0500

    added fungus for sentient plant dialog

---
+++ Game Studio 2/Assets/Scripts/SentientPlant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class SentientPlant : Tile
+{
+    public Flowchart flowchart;
+
+    public override void OnTileEnter()
+    {
+        player.timer.AddTime(1); // talking to plants takes time i guess
+        flowchart.ExecuteBlock("PlantTalk");
+    }
+
+    public override void OnTileExit(){
+        
+    }
+}
