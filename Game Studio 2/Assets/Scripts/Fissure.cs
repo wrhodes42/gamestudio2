e7f8b00b8f76b824e3d3f82dfc4cc8b9e0b857da
Will Rhodes <wrhodes42@gmail.com>
2020-02-12 20:14:25 -0500

    add fissures

---
+++ Game Studio 2/Assets/Scripts/Fissure.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fissure : Tile
+{
+    List<Transform> allFissures = new List<Transform>();
+
+    public void Start()
+    {
+        
+    }
+
+    public override void OnTileEnter()
+    {
+        player.timer.AddTime(1); // swimming takes time i guess
+    }
+
+    public override void OnTileExit()
+    {
+        
+    }
+}
